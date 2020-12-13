98eb1ae17d5cb75b9ba24b72ba5d90dcd498ee99
Saba <saba.saghinadze@wandio.com>
2020-12-14 00:05:17 +0400

    finished

--- Assignment5/Yahtzee.java
+++ Assignment5/Yahtzee.java
@@ -7,36 +7,111 @@
 import acm.io.*;
 import acm.program.*;
 import acm.util.*;
-
 import java.util.*;
 
 public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
-	
+
 	public static void main(String[] args) {
 		new Yahtzee().start(args);
 	}
-	
+
 	public void run() {
-		IODialog dialog = getDialog();
-		nPlayers = dialog.readInt("Enter number of players");
-		playerNames = new String[nPlayers];
-		for (int i = 1; i <= nPlayers; i++) {
-			playerNames[i - 1] = dialog.readLine("Enter name for player " + i);
-		}
+		readPlayersNumber();
+		readPlayersNames();
 		display = new YahtzeeDisplay(getGCanvas(), playerNames);
 		categorySelected = new boolean[nPlayers][N_CATEGORIES];
+		scores = new int[nPlayers][N_CATEGORIES];
 		playGame();
 	}
 
+	/** Read players number which is between 1 and 4 */
+	private void readPlayersNumber(){
+		while(true){
+			int playersNumber = getDialog().readInt("Enter number of players");
+			if(playersNumber > 0 && playersNumber < 5) {
+				nPlayers = playersNumber;
+				break;
+			}else{
+				getDialog().showErrorMessage("Please, enter values between 1 and 4");
+			}
+		}
+	}
+
+	/** Read players' names */
+	private void readPlayersNames(){
+		playerNames = new String[nPlayers];
+		for (int i = 1; i <= nPlayers; i++) {
+			while(true){
+				String playerName = getDialog().readLine("Enter name for player " + i);
+				if(playerName.length() > 0){
+					playerNames[i - 1] = playerName;
+					break;
+				}else{
+					getDialog().showErrorMessage("Please, enter a valid name");
+				}
+			}
+		}
+	}
+
+	/** Play game */
 	private void playGame() {
 		for(int i=0; i<N_SCORING_CATEGORIES; i++){
 			for(int j=1; j<=nPlayers; j++){
 				playerTurn(j);
 			}
 		}
+
+		calculateScores();
 	}
 
+	/** Calculate scores sum and update scores' table */
+	private void calculateScores() {
+		int[] upperScore = new int[nPlayers];
+		int[] upperBonus = new int[nPlayers];
+		int[] lowerScore = new int[nPlayers];
+		int[] total = new int[nPlayers];
+		int winnerIndex = 0;
+		int maxScore = 0;
+
+		for(int i=0; i<nPlayers; i++){
+			for(int j=0; j<7; j++){
+				upperScore[i] += scores[i][j];
+				total[i] += scores[i][j];
+			}
+
+			for(int j=7; j<N_CATEGORIES; j++){
+				lowerScore[i] += scores[i][j];
+				total[i] += scores[i][j];
+			}
+
+			if(upperScore[i] >= 63) {
+				upperBonus[i] = 35;
+				total[i] += 35;
+			}
+
+			if(total[i] > maxScore){
+				winnerIndex = i;
+				maxScore = total[i];
+			}
+
+			display.updateScorecard(UPPER_SCORE, i+1, upperScore[i]);
+			display.updateScorecard(UPPER_BONUS, i+1, upperBonus[i]);
+			display.updateScorecard(LOWER_SCORE, i+1, lowerScore[i]);
+			display.updateScorecard(TOTAL, i+1, total[i]);
+		}
+
+		display.printMessage(
+				"Congratulations, "
+						+ playerNames[winnerIndex]
+						+ ", you're the winner with a total score of "
+						+ maxScore
+						+ "!"
+		);
+	}
+
+	/** Player turn */
 	private void playerTurn(int playerIndex){
+		display.printMessage(playerNames[playerIndex-1] + "'s turn. Click \"Roll Dice\" button to roll the dice.");
 		display.waitForPlayerToClickRoll(playerIndex);
 
 		int[] diceNumbers = new int[N_DICE];
@@ -44,15 +119,20 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		for(int i=1; i<=3; i++){
 			roll(diceNumbers, i);
 			display.displayDice(diceNumbers);
-			if(i!=3) display.waitForPlayerToSelectDice();
+			if(i!=3) {
+				display.printMessage("Select the dice you wish to re-roll and click \"Roll Again\"");
+				display.waitForPlayerToSelectDice();
+			}
 		}
 
 		while(true){
+			display.printMessage("Select a category for this roll");
 			int categoryNumber = display.waitForPlayerToSelectCategory();
 			int score = getScore(categoryNumber, diceNumbers);
 
 			if(!categorySelected[playerIndex-1][categoryNumber]){
 				categorySelected[playerIndex-1][categoryNumber] = true;
+				scores[playerIndex-1][categoryNumber] = score;
 				display.updateScorecard(categoryNumber, playerIndex, score);
 				break;
 			}else{
@@ -62,6 +142,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 
 	}
 
+	/** Roll a dice */
 	private void roll(int[] diceNumbers, int rollNumber){
 		for(int i=0; i<N_DICE; i++){
 			if(display.isDieSelected(i) || rollNumber == 1){
@@ -70,6 +151,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		}
 	}
 
+	/** Returns score for specific dice combination */
 	private int getScore(int categoryNumber, int[] diceNumbers){
 		Arrays.sort(diceNumbers);
 
@@ -89,12 +171,11 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 			case CHANCE -> {
 				return java.util.stream.IntStream.of(diceNumbers).sum();
 			}
-			default -> {
-				throw new ErrorException("checkCategory: Illegal category");
-			}
+			default -> throw new ErrorException("checkCategory: Illegal category");
 		}
 	}
 
+	/** Check different combinations */
 	private int straightCase(int categoryNumber, int[] diceNumbers){
 		boolean temp = false;
 		for(int i=0; i<N_DICE-1; i++){
@@ -148,11 +229,12 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		}
 		return score;
 	}
-		
-/* Private instance variables */
+
+	/* Private instance variables */
 	private int nPlayers;
 	private String[] playerNames;
-	private YahtzeeDisplay display;	
-	private RandomGenerator rgen = new RandomGenerator();
+	private YahtzeeDisplay display;
+	private final RandomGenerator rgen = new RandomGenerator();
 	private boolean[][] categorySelected;
+	private int[][] scores;
 }
